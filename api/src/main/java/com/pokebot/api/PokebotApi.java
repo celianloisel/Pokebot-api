b6697eeb5d606c8595acbf4b600e50d7a46c1556
celianloisel <celianloisel@gmail.com>
2023-09-06 17:22:08 +0200

    feat: setting up api

---
+++ api/src/main/java/com/pokebot/api/PokebotApi.java
@@ -0,0 +1,13 @@
+package com.pokebot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokebotApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokebotApi.class, args);
+	}
+
+}
