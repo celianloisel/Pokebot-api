2e9a9ce59c53c1e4b5906c58256a56ec2f8fa4e4
celianloisel <celianloisel@gmail.com>
2023-09-07 22:39:17 +0200

    feat: create other endpoint to get card info

--- api/src/main/java/com/pokebot/api/service/getCardInfo/GetCardInfoServiceImpl.java
+++ api/src/main/java/com/pokebot/api/service/getCardInfo/GetCardInfoServiceImpl.java
@@ -16,8 +16,14 @@ public class GetCardInfoServiceImpl implements GetCardInfoService {
         this.restTemplate = restTemplate;
     }
 
-    public String getCardInfo(String set, String cardLocalId) {
+    public String getCardInfoWithSetAndLocalId(String set, String cardLocalId) {
         String apiUrl = String.format("%s/v2/fr/sets/%s/%s", tcgdexBaseUrl, set, cardLocalId);
+
+        return restTemplate.getForObject(apiUrl, String.class);
+    }
+
+    public String getCardInfoWithId(String id) {
+        String apiUrl = String.format("%s/v2/fr/cards/%s", tcgdexBaseUrl, id);
 
         return restTemplate.getForObject(apiUrl, String.class);
     }
