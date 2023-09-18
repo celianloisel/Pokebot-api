9a98c548f3d4ae465fbb0ae09b67e0b1a427a3ad
celianloisel <celianloisel@gmail.com>
2023-09-18 20:55:00 +0200

    fix : return error if card is not found

---
+++ api/src/main/java/com/pokebot/api/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.pokebot.api.config.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
+
--- api/src/main/java/com/pokebot/api/service/getCardInfo/GetCardInfoServiceImpl.java
+++ api/src/main/java/com/pokebot/api/service/getCardInfo/GetCardInfoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.pokebot.api.service.getCardInfo;
 
+import com.pokebot.api.config.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -20,12 +22,20 @@ public class GetCardInfoServiceImpl implements GetCardInfoService {
     public String getCardInfoWithSetAndLocalId(String set, String cardLocalId) {
         String apiUrl = String.format("%s/%s/fr/sets/%s/%s", tcgdexBaseUrl, tcgdexVersion, set, cardLocalId);
 
-        return restTemplate.getForObject(apiUrl, String.class);
+        try {
+            return restTemplate.getForObject(apiUrl, String.class);
+        } catch (HttpClientErrorException.NotFound ex) {
+            throw new ResourceNotFoundException("La carte avec le set " + set + " et l'ID local " + cardLocalId + " n'a pas été trouvée.");
+        }
     }
 
     public String getCardInfoWithId(String id) {
         String apiUrl = String.format("%s/%s/fr/cards/%s", tcgdexBaseUrl, tcgdexVersion, id);
 
-        return restTemplate.getForObject(apiUrl, String.class);
+        try {
+            return restTemplate.getForObject(apiUrl, String.class);
+        } catch (HttpClientErrorException.NotFound ex) {
+            throw new ResourceNotFoundException("La carte avec l'ID " + id + " n'a pas été trouvée.");
+        }
     }
 }
